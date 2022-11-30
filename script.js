4bfa35e0c859b42998acec5f3aa8dbbc8adebdb8
Fred <frederico.henrique.1992@gmail.com>
2022-11-30 19:51:52 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+var arrayValues = []
+var result = ''
+
 function calcular(type, value) {
     if (type == 'action') {
+        if (value == 'c') {
+            document.getElementById('show-result').value = ''
+        }
 
-    } else if (type == 'value') {
-        document.getElementById('show-result').value = value
+        if (value == '.') {
+            // Validar se já há . na string e não incluir um novo .
+            var aux = document.getElementById('show-result').value += value
+        }
+
+        if (value == '/' || value == '*' || value == '-' || value == '+') {
+            arrayValues.push(document.getElementById('show-result').value)
+            arrayValues.push(value)
+            document.getElementById('show-result').value = ''
+        }
+
+        if (value == '=') {
+            if (arrayValues.length != 0) {
+                arrayValues.push(document.getElementById('show-result').value)
+                for (let index = 0; index < arrayValues.length; index++) {
+                    result += arrayValues[index];
+                }
+                arrayValues = []
+                document.getElementById('show-result').value = eval(result)
+                result = ''
+            }
+
+        }
+
+    } else if (type == 'number') {
+        document.getElementById('show-result').value += value
     }
-
-
-
-
 }
